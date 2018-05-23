fff526dc9fd4b5862a1fdf6cb56e18fc3eca652f
Kirtaner <acottle@taimalabs.com>
2018-05-22 23:11:58 -0400

    es6 hurr

--- netjester.js
+++ netjester.js
@@ -1,52 +1,37 @@
-module.exports = function(io, config) {
-    let Connected = {},
-        lastResponse = '',
-        clientConfig = {
-            "Voice": config.get('Voice'),
-            "Netjester": config.get('Netjester'),
-            "Debug": config.get('Server.debug')
-        };
-
-    // espeak, festival, flite, freetts, mimic
-
-    Netjester = {
-        initializeClient(client) {
-            client.emit('connected', clientConfig);
-        },
-
-        // Basically just forwards the 420chan Netjester API response to the browser client for TTS
-        // Twitch chat -> Phantombot -> 420chan API -> Netjester AI daemon -> back to Phantombot -> Here/channel
-        sendNextResponse() {
-            // io.emit()
-        },
-
-        pollChatResponse() {
-
-        },
-
-        systemSpeechFallback() {
-            
-        }
+NetjesterServer = (io, config) => {
+    lastResponse = '';
+    clientConfig = {
+        "Voice": config.get('Voice'),
+        "Netjester": config.get('Netjester'),
+        "Debug": config.get('Server.debug')
     };
 
-    // Realistically only one connection will ever be alive at a time, at least in this incarnation
-    // Still have to handle graceful termination and all that crap tho
-    io.on('connection', function (client) {
-        Connected[client.id] = client;
-        console.log("Clients connected: %d", Object.keys(Connected).length);
+    // Basically just forwards the 420chan Netjester API response to the browser client for TTS
+    // Twitch chat -> Phantombot -> 420chan API -> Netjester AI daemon -> back to Phantombot -> Here/channel
+    sendNextResponse = () => {
+    };
 
-        Netjester.initializeClient(client);
+    pollChatResponse = () => {
+    };
 
-        client.on('disconnect', function() {
-            delete Connected[client.id];
-            console.log('Client disconnected');
-        });
+    systemSpeechFallback = () => {
+    };
 
-        client.on('finishedTalking', function(response) {
-            // give a bit of thought to voice queueing and rate limiting
-        });
+    connectClient = (client) => {
+        client.emit('connected', clientConfig);
 
-        // Use node server and local system processes if wanted/needed
-        client.on('fallbackToSystemSpeech', Netjester.systemSpeechFallback);
-    });
+        client.on('disconnect', disconnectClient);
+        client.on('finishedTalking', sendNextResponse);
+        client.on('fallbackToSystemSpeech', systemSpeechFallback);
+
+        console.log("Clients connected: %d", io.engine.clientsCount);
+    };
+
+    disconnectClient = (client) => {
+        console.log('Client disconnected');
+    };
+
+    io.on('connection', connectClient);
 };
+
+module.exports = NetjesterServer;
